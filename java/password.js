f8690a4bc29c53ca0de0e093f611ffd83182fcbe
rbkoho <taylorbruno895@gmail.com>
2019-10-26 23:46:37 -0400

    began java. created variables and functions for password length.

--- java/password.js
+++ java/password.js
@@ -1,11 +1,30 @@
-var howMany ;
+var x = 0;
+var howMany = 0;
 
-var specChar ;
+document.getElementById("passwordClick").addEventListener("click", passwordClick);
 
-
-function passwordClick(){
-prompt("Please select between 8 and 128 characters."); 
-return howMany;
+function passwordClick() {
+    document.getElementById("passwordClick");
+    charNum();
 };
 
-function clipboardClick
+function charNum() {
+    var x = prompt("Please select between 8 and 128 characters.");
+    if ((x < "8") && (x > "128")) {
+        alert("Try again");
+    } else {
+        var howMany = (parseInt(x)+0);
+        isSpecTrue();
+    }
+    console.log(howMany + " characters.");
+};
+function isSpecTrue() {
+    var specChar = prompt("Press OK if you would like special characters. Otherwise, cancel.");
+    if (specChar) {
+        
+        isCapTrue();
+    }  else {
+        isCapTrue();
+    }
+}
+// function clipboardClick();
